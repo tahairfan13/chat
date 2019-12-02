aee4f8d7b5851a2fb522245ecb20a4aa13d9cb0e
Taha Ali <tahairfan1993@gmail.com>
2019-12-02 20:50:28 +0500

    initial commit

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express')
 const app = express()
 const socketio = require('socket.io')
 
-//app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 
 
-// app.get('/', (req, res) => {
-//     res.send("HELLOW")
-// })
+app.get('/', (req, res) => {
+    res.send("HELLOW")
+})
+
+app.post('/send_message', (req, res) => {
+
+})
 
 const expressServer = app.listen(3000)
 const io = socketio(expressServer)
-console.log("HI")
 io.on('connection', (socket) => {
     socket.emit('messageFromServer', {data: "Welcome to SocketIO server" })
     socket.on('messageToServer', (dataFromClient) => {
